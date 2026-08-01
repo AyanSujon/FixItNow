caddecd53b69fece6aec60b693cd8395672fc240
Ayan <sujon.seo.bd@gmail.com>
2026-08-01 15:48:02 +0600

    feat: crate service form added in the technician deshboard

--- src/app/(dashboard)/technician-dashboard/services/create/page.tsx
+++ src/app/(dashboard)/technician-dashboard/services/create/page.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+// import React from 'react'
 
-export default function ServiceCreatePage() {
-  return (
-    <div>ServiceCreatePage</div>
-  )
+
+// export default function ServiceCreatePage() {
+//   return (
+//     <div>ServiceCreatePage</div>
+//   )
+// }
+
+
+
+import CreateServiceForm from "@/app/(dashboard)/_components/CreateServiceForm";
+import { getAllCategories } from "@/app/(public)/_actions/getAllCategories";
+
+
+
+export default async function ServiceCreatePage() {
+    const result = await getAllCategories();
+
+    // console.log("Fetched categories:", result.data);
+    return (
+        <div className="mx-auto max-w-4xl space-y-6">
+        <CreateServiceForm
+          categories={result.data ?? []}
+        />
+
+        </div>
+    );
 }
